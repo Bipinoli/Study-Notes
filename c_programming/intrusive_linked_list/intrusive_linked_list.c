a669467677c4954da3ff8772bc49ad0ca06366e6
Bipin Oli <bipinoli90@gmail.com>
2025-12-05 14:30:56 +0100

    intrusive linked list

--- c_programming/intrusive_linked_list/intrusive_linked_list.c
+++ c_programming/intrusive_linked_list/intrusive_linked_list.c
@@ -10,11 +10,9 @@ struct list_node {
   struct list_node *prev;
 };
 
-struct list_node create_list_node() {
-  struct list_node cur;
-  cur.next = &cur;
-  cur.prev = &cur;
-  return cur;
+void init_list_node(struct list_node *self) {
+  self->next = self;
+  self->prev = self;
 }
 
 void push_back_list_node(struct list_node *head, struct list_node *new_node) {
@@ -51,7 +49,22 @@ struct list {
   void (*push_back)(struct list *self, int a);
   void (*pop_back)(struct list *self);
   int (*back)(struct list *self);
+  bool (*empty)(struct list *self);
+  void (*destroy)(struct list *self);
 };
+
+
+void destroy(struct list *self) {
+  struct list_node *cur = self->head.next;
+  struct list_node *next;
+  while (cur != &(self->head)) {
+    next = cur->next;
+    struct list *cur_cont = container_of(cur, struct list, head);
+    free(cur_cont);
+    cur = next;
+  }
+}
+
 
 void push_back(struct list *self, int a) {
   struct list *new_item = malloc(sizeof(struct list));
@@ -60,8 +73,18 @@ void push_back(struct list *self, int a) {
     exit(1);
   }
   new_item->value = a;
-  new_item->head = create_list_node();
+  init_list_node(&(new_item->head));
   push_back_list_node(&(self->head), &(new_item->head));
+}
+
+void pop_back(struct list *self) {
+  if (self->empty(self)) {
+    return;
+  }
+  struct list_node *last = self->head.prev;
+  pop_back_list_node(&(self->head));
+  struct list *last_item = container_of(last, struct list, head);
+  free(last_item);
 }
 
 int back(struct list *self) {
@@ -74,17 +97,47 @@ int back(struct list *self) {
 }
 
 
-struct list create_list() {
-  struct list retval;
-  retval.head = create_list_node();
-  retval.push_back = push_back;
-  retval.back = back;
-  return retval;
+bool empty(struct list *self) {
+  return empty_list_node(&(self->head));
 }
 
+
+
+void init_list(struct list *self) {
+  init_list_node(&(self->head));
+  self->push_back = push_back;
+  self->pop_back = pop_back;
+  self->back = back;
+  self->empty = empty;
+  self->destroy = destroy;
+}
+
+
+void display_list(struct list *self) {
+  struct list_node *cur = self->head.next;
+  while (cur != &(self->head)) {
+    printf("%d -> ", (container_of(cur, struct list, head))->value);
+    cur = cur->next;
+  }
+  printf("\n");
+}
+
+
+
+
 int main() {
-  struct list lst = create_list();
-  lst.push_back(&lst, 1);
-  printf("last: %d\n", lst.back(&lst));
+  struct list lst;
+  init_list(&lst);
+  for (int i=1; i<10; i++) {
+    lst.push_back(&lst, i);
+    display_list(&lst);
+    printf("Back: %d\n", lst.back(&lst));
+  }
+  while (!lst.empty(&lst)) {
+    lst.pop_back(&lst);
+    display_list(&lst);
+    printf("Back: %d\n", lst.back(&lst));
+  }
+  destroy(&lst);
   return 0;
 }
